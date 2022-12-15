a023d23b4333581a762f427a1b285340ced3a940
viratsathish1999 <sathish.r9688@gmail.com>
2022-12-15 14:59:18 +0530

    add file

---
+++ main.js.js
@@ -0,0 +1,15 @@
+var a,b,c;
+a=prompt("enter first value");
+b=prompt("enter second value");
+function swap(a,b){
+  c=a
+  a=b
+  b=c
+  document.write(a)
+  document.write(b)
+ 
+  
+  
+
+}
+swap(a,b)
